6b0809e5a5fc549391f3b1d0532e323a27f44eca
rilane <s10474@pjwstk.edu.pl>
2017-12-27 03:59:54 +0100

    set EntityModel as parcelable

--- app/src/main/java/kot/helena/chancebalancer/EntityModel.kt
+++ app/src/main/java/kot/helena/chancebalancer/EntityModel.kt
@@ -1,6 +1,9 @@
 package kot.helena.chancebalancer
 
-class EntityModel {
+import android.os.Parcel
+import android.os.Parcelable
+
+class EntityModel() : Parcelable{
     var name: String = ""
     var attackChance: Int = 0
     var attackDamage: Int = 0
@@ -8,4 +11,38 @@ class EntityModel {
     var defenceTimes: Int = 0
     var health: Int = 0
     var armor: Int = 0
+
+    constructor(parcel: Parcel) : this() {
+        name = parcel.readString()
+        attackChance = parcel.readInt()
+        attackDamage = parcel.readInt()
+        defenceChance = parcel.readInt()
+        defenceTimes = parcel.readInt()
+        health = parcel.readInt()
+        armor = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeInt(attackChance)
+        parcel.writeInt(attackDamage)
+        parcel.writeInt(defenceChance)
+        parcel.writeInt(defenceTimes)
+        parcel.writeInt(health)
+        parcel.writeInt(armor)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<EntityModel> {
+        override fun createFromParcel(parcel: Parcel): EntityModel {
+            return EntityModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<EntityModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
